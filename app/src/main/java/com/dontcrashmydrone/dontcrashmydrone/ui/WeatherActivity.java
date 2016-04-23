637f593070e8dee1858284713b421ae10ad35c39
Sunny Raj <sundarcs@gwu.edu>
2016-04-23 19:15:06 -0400

    Add Weather design

--- app/src/main/java/com/dontcrashmydrone/dontcrashmydrone/ui/WeatherActivity.java
+++ app/src/main/java/com/dontcrashmydrone/dontcrashmydrone/ui/WeatherActivity.java
@@ -1,7 +1,30 @@
 package com.dontcrashmydrone.dontcrashmydrone.ui;
 
-/**
- * Created by sunny on 4/23/16.
- */
-public class WeatherActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.dontcrashmydrone.dontcrashmydrone.R;
+
+import butterknife.Bind;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    public static final String TAG = WeatherActivity.class.getSimpleName();
+    public static final String HOURLY_FORECAST = "HOURLY_FORECAST";
+    //private Forecast mForecast;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+
+    private void getForecast(double latitude, double longitude) {
+        String apiKey = "e7220de7fb16ee318ac979fd820daf74";
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
+    }
 }
