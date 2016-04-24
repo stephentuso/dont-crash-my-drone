056e600dbb1096720e284ad4932e57685c20ffe0
Sunny Raj <sundarcs@gwu.edu>
2016-04-23 20:05:14 -0400

    Add ui for weather

---
+++ app/src/main/java/com/dontcrashmydrone/dontcrashmydrone/weather/Forecast.java
@@ -0,0 +1,18 @@
+package com.dontcrashmydrone.dontcrashmydrone.weather;
+
+/**
+ * Created by sunny on 4/23/16.
+ */
+
+public class Forecast {
+
+    private Current mCurrent;
+
+    public Current getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(Current current) {
+        mCurrent = current;
+    }
+}
